9eb676b10f2c4f3af2cfad18e1af4c0fc77f6807
Tom Stine <13zebras@users.noreply.github.com>
2024-04-30 20:14:37 -0500

    Create 010-constants.sol

---
+++ solidity-solutions/010-constants.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.24;
+
+contract Constants {
+    address public constant MY_ADDR = 0x777788889999AaAAbBbbCcccddDdeeeEfFFfCcCc;
+
+    uint256 public MY_UINT = 123;
+}
